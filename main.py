1a1c2a27b9e90f6e3ab11304a36907c6fc6f2f80
Axel Tifrani <axeltifrani@gmail.com>
2024-04-04 16:27:20 +0200

    add get specific user with id

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # import du package fastapi
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User, Gender, Role
-from uuid import uuid4
+from uuid import uuid4, UUID
 from typing import List
 
 #initialisation de l'application
@@ -49,3 +49,15 @@ def get_users():
 def add_users(user:User):
     db.append(user)
     return 'User succesfully added'
+
+# Creation d'une methode get d'un user par id
+@app.get("/api/v1/user/{id}")
+def get_user(id:UUID):
+    for user in db:
+        if user.id == id:
+            return user
+       
+    raise HTTPException(
+        status_code= 404,
+        detail= f"Error 404. User with id {id}, not found."
+        )
